0c482bd385bff8098447950945c8815e8cbbb6bc
Huziieditz <163836074+Huziieditz@users.noreply.github.com>
2024-03-19 16:40:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '💝 Mr Huzii 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
+global.packname = '💝 Mr Huzii 💝 Created By 💝 Huzaifa Official²⁰²³ 💝' 
+global.author = '💝 Huzaifa Official²⁰²³ 💝' 
 global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
 global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
@@ -59,8 +59,8 @@ global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
-global.rwait = '🇱🇰'
+global.wait = '*💝 _Waiting Bro Uploading by 💝 Huzaifa 💝..._*'
+global.rwait = '🇵🇰'
 global.dmoji = '🤣'
 global.done = '👍'
 global.error = '👎' 
